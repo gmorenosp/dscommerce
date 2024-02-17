ec4c299978a6042abdcdef4df6ac4342e34f5ff0
Gustavo Moreno <gmorenosmail@gmail.com>
2024-02-17 06:22:24 -0300

    ProductMinDTO

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
@@ -33,10 +34,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findALL(String name, Pageable pageable){
+    public Page<ProductMinDTO> findALL(String name, Pageable pageable){
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
-
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
