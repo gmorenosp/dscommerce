cc2de6566fa5b632374d0a1576b80c153e6fff7f
Gustavo Moreno <gmorenosmail@gmail.com>
2024-02-16 21:59:30 -0300

    Access control

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -58,8 +58,8 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$CRZZXH8.r9zBgD/HyW682.IgcLxTsRSA3wlOdr2NCmcMY503EgYs2', '2001-07-25');
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$CRZZXH8.r9zBgD/HyW682.IgcLxTsRSA3wlOdr2NCmcMY503EgYs2', '1987-12-13');
 
-INSERT INTO tb_role (authoriry) VALUES ('ROLE_CLIENT');
-INSERT INTO tb_role (authoriry) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1,1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2,1);
