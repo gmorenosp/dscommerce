59dd2a2264ff9348982cf7ac3b73ba2205f846d4
Gustavo Moreno <gmorenosmail@gmail.com>
2024-02-17 21:21:18 -0300

    Dev profile

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -92,4 +93,16 @@ public class Product {
         return items.stream().map(x -> x.getOrder()).toList();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
